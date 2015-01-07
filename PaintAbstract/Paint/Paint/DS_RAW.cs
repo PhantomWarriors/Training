60590a396d2bfae02b153fb51898b05f5c08a185
Nishik <nimbik@gmail.com>
2015-01-08 12:35:07 +1300

    PaintAbstract

---
+++ PaintAbstract/Paint/Paint/DS_RAW.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint
+{
+    class DS_RAW : iDS
+    {
+       
+        //protected iDS Next;
+
+        //iDS iDS.Next
+        //{
+        //    get
+        //    {
+        //        return Next;
+        //    }
+        //    set
+        //    {
+        //        Next = value;
+        //    }
+        //}
+
+
+        public override iDS Get(string fileName)
+        {
+            iDS res=null;
+            if (fileName == ".raw")
+            {
+                res = new DS_Standard();
+            }
+            else
+            {
+                res = null;
+            }
+            return res;
+        }
+        public override void Save(Bitmap bmp, string fileName)
+        {
+            using (var ms = new MemoryStream())
+            {
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Tiff);
+                byte[] imageBytes = ms.ToArray();
+                string base64string = Convert.ToBase64String(imageBytes);
+                File.WriteAllText(fileName, base64string);
+            }
+
+             //using(MemoryStream ms = new MemoryStream())
+             //{
+             //       bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            //      Byte[] data = new byte[ms.Length];
+             //       data = ms.ToArray();
+             //       File.WriteAllBytes(fileName, data);
+
+             //}
+
+        }
+        public override Bitmap Load(string fileName)
+        {
+            byte[] imageBytes = Convert.FromBase64String(File.ReadAllText(fileName));
+            MemoryStream ms = new MemoryStream(imageBytes, 0,imageBytes.Length);
+
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+            Bitmap b = (Bitmap)image;
+
+            return b;
+            
+            //Image img = null;
+
+            //byte[] imageData = null;
+            //FileInfo fileInfo = new FileInfo(fileName);
+            //FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            //BinaryReader br = new BinaryReader(fs);
+            //imageData = br.ReadBytes((int)fs.Length);
+
+
+            //using (var mss = new MemoryStream(imageData))
+            //{
+            //    img = Image.FromStream(mss,true,true);
+            //    bm = new Bitmap(img);
+
+            //}
+            
+            //byte[] imageDate = null;
+            //Image img = null;
+            //Bitmap b = null;
+
+            //using (var ms = new MemoryStream())
+            //{
+
+            //    using (var f = File.OpenRead(fileName))
+            //    {
+            //        byte[] buffer = new byte[f.Length];
+            //        int read;
+            //        while ((read = f.Read(buffer, 0, buffer.Length)) > 0)
+            //        {
+            //            ms.Write(buffer, 0, read);
+            //        }
+            //    }
+            //    imageDate = ms.ToArray();
+            //}
+
+            //using (var mss = new MemoryStream(imageDate))
+            //{
+            //    img = Image.FromStream(mss);
+            //    b = new Bitmap(img);
+
+            //}
+            //return b;
+        }
+        public override iDS IsReady(string extension)
+        {
+            iDS res = null;
+            if (extension == ".raw")
+            {
+                res = new DS_RAW();
+            }
+            else
+            {
+                res = this.Next.IsReady(extension); 
+            }
+            return res;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ PaintAbstract/Paint/Paint/Factory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint
+{
+     static class Factory
+    {
+        static private List<iDS> ds;
+        static iDS res, ress;
+
+        static public iDS Get(string type)
+        {          
+            if (ds == null)
+            {
+                Init();
+            }
+            foreach (var dss in ds)
+            {
+                res = dss.Get(type);
+                if (res != null)
+                {
+                    ress = res;
+                }
+            }
+            return ress;
+
+            //switch (type)
+            //{
+            //    case "pdf":
+            //        return new DS_PDF();
+            //    case "raw":
+            //        return new DS_RAW();
+            //    case "psd":
+            //        return new DS_PSD();
+            //    case "png":
+            //    case "bmp":
+            //    case "jpg":
+            //    default:
+            //        return new DS_Standard();
+            //}
+        }
+        static private void Init()
+        {
+            ds = new List<iDS>();
+            ds.Add(new DS_Standard());
+            ds.Add(new DS_RAW());
+            ds.Add(new DS_PSD());
+            ds.Add(new DS_PDF());
+        }
+        static public string GetFilterForSave()
+        {
+            return "JPG (*.jpg)|*.jpg;|BMP (*.bmp)|*.bmp;|PNG (*.png)|*.png;|PDF (*.pdf)|*.pdf;|RAW (*.raw)|*.raw;|PSD(*.psd)|*.psd;";
+        }
+        static public string GetFilterForLoad()
+        {
+            return "JPG(*.jpg)|*.jpg;|BMP(*.bmp)|*.bmp;|PNG(*.png)|*.png; |RAW(*.raw)|*.raw;|PSD(*.psd)|*.psd;";
+        }
+    }
+}
